de048f6ef932767d8c851e276520b9be1a5a4195
YOrtiz <yortiz@blumonpay.com.mx>
2023-09-27 16:01:20 -0600

    Version 1.0.0 - °Se inicia proyecto y se inicializa el primer layaout y las primeras pages

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import NuevoCliente from './pages/NuevoCliente';
+import Index from './pages/Index';
+
+const router = createBrowserRouter([
+  {
+    //Layout pricipal compartido en todas las vistas
+    path: '/',
+    element: <Layout />,
+    //Elementos hijos que se inyectan al Outlet del elemento padre en este caso Layout
+    children: [
+      {
+        //Elemento hijo de la ruta index
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
